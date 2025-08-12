734b79dde13c4e980f9cad851e1503d5069084fd
Andrey Tarasov <andvatar@gmail.com>
2025-08-13 00:52:42 +0300

    kafka chart

--- exchangeGenerator/src/main/java/ru/yandex/practicum/tarasov/exchangegenerator/configuration/KafkaConfig.java
+++ exchangeGenerator/src/main/java/ru/yandex/practicum/tarasov/exchangegenerator/configuration/KafkaConfig.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.tarasov.exchangegenerator.configuration;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.Map;
 
