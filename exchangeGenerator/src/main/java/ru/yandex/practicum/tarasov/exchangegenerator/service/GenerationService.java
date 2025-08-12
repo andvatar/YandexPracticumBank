87f77f780d00daec02b3356c5e2cf69194159971
Andrey Tarasov <andvatar@gmail.com>
2025-08-12 23:59:11 +0300

    kafka chart

--- exchangeGenerator/src/main/java/ru/yandex/practicum/tarasov/exchangegenerator/service/GenerationService.java
+++ exchangeGenerator/src/main/java/ru/yandex/practicum/tarasov/exchangegenerator/service/GenerationService.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.tarasov.exchangegenerator.service;
 
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.tarasov.exchangegenerator.DTO.ResponseDto;
@@ -8,13 +9,17 @@ import ru.yandex.practicum.tarasov.exchangegenerator.client.dto.ExchangeRate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GenerationService {
     private final ExchangeClient  exchangeClient;
+    private final KafkaTemplate<String, List<ExchangeRate>> kafkaTemplate;
 
-    public GenerationService(ExchangeClient exchangeClient) {
+    public GenerationService(ExchangeClient exchangeClient,
+                             KafkaTemplate<String, List<ExchangeRate>> kafkaTemplate) {
         this.exchangeClient = exchangeClient;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @Scheduled(fixedRate = 60000)
@@ -24,6 +29,11 @@ public class GenerationService {
         rates.add(new ExchangeRate("USD", "US Dollar", Math.round(Math.random() * 10000)/100.0));
         rates.add(new ExchangeRate("CNY", "Chinese yuan", Math.round(Math.random() * 10000)/100.0));
 
-        ResponseDto responseDto = exchangeClient.addRates(rates);
+        //ResponseDto responseDto = exchangeClient.addRates(rates);
+
+        kafkaTemplate.send("exchange-rates-topic",
+                rates.stream().map(ExchangeRate::getCurrencyCode).collect(Collectors.joining()),
+                rates);
+
     }
 }
